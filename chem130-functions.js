4dcfb757a38b70337575b73c2f849bb84544f516
Adris <adrisj7@gmail.com>
2018-11-03 16:33:02 -0400

    Update parsing data to not be garbage

--- chem130-functions.js
+++ chem130-functions.js
@@ -1,4 +1,5 @@
 
+
 // Grab element data from here
 var elementURL = "https://raw.githubusercontent.com/andrejewski/periodic-table/master/data.csv";
 
@@ -12,21 +13,23 @@ $(document).ready(function() {
 });
 
 function processData(data) {
-    console.log("Data: " + data);
-    var rows = data.split("\n");
+	var rows = data.split("\n");
     // Skip the top row which has column descriptions
     for(var i = 1; i < rows.length; i++) {
         var cols = rows[i].split(",");
         var symbol = cols[1];
         var name = cols[2];
         var mass = parseFloat(cols[3]);
-
+		if (symbol != undefined) {
+			symbol = symbol.trim();
+        }
+		if (name != undefined) {
+			name = name.trim();
+        }
         if (isNaN(mass)) mass = -1;
-
         window[symbol] = {
             "name" : name,
             "mass" : mass
         };
     }
 }
-
